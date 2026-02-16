2a8cd264f135260e5a47acfa0f574b8fef85f018
kostin-alex <aleksandr.kostin@gmail.com>
2026-02-16 20:02:40 +0200

    added keyboards

--- create_bot.py
+++ create_bot.py
@@ -5,7 +5,6 @@ from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
-# from decouple import config
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 from dotenv import load_dotenv
@@ -18,7 +17,8 @@ PG_LINK = os.environ["PG_LINK"]
 print(PG_LINK)
 # pg_db = PostgresHandler(PG_LINK)
 scheduler = AsyncIOScheduler(timezone='Europe/Kyiv')
-admins = []  # [int(admin_id) for admin_id in config('ADMINS').split(',')]
+admins = [int(admin_id) for admin_id in os.environ['ADMINS'].split(',')]
+print(admins)
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
--- handlers/start.py
+++ handlers/start.py
@@ -1,20 +1,43 @@
 from aiogram import Router, F
-from aiogram.filters import CommandStart, Command
+from aiogram.filters import CommandStart, Command, CommandObject
 from aiogram.types import Message
+from aiogram.types import BotCommand, BotCommandScopeDefault
+from keyboards.all_kb import main_kb, create_spec_kb, create_rat
+
 
 start_router = Router()
 
 
 @start_router.message(CommandStart())
-async def cmd_start(message: Message):
-    await message.answer('Запуск сообщения по команде /start используя фильтр CommandStart()')
+async def cmd_start(message: Message, command: CommandObject):
+    command_args: str = command.args
+    if command_args:
+        await message.answer(
+            f'Запуск сообщения по команде /start используя фильтр CommandStart() с меткой <b>{command_args}</b>',
+            reply_markup=main_kb(message.from_user.id))
+    else:
+        await message.answer(
+            'Запуск сообщения по команде /start используя фильтр CommandStart() без метки',
+            reply_markup=main_kb(message.from_user.id))
 
 
 @start_router.message(Command('start_2'))
 async def cmd_start_2(message: Message):
-    await message.answer('Запуск сообщения по команде /start_2 используя фильтр Command()')
+    await message.answer(
+        'Запуск сообщения по команде /start_2 используя фильтр Command()',
+        reply_markup=create_spec_kb())
 
 
 @start_router.message(F.text == '/start_3')
 async def cmd_start_3(message: Message):
-    await message.answer(f'Запуск сообщения по команде /start_3 используя магический фильтр F.text! {message.from_user.id} {message.from_user.username} {message.from_user.first_name} {message.from_user.last_name}')
+    await message.answer(
+        f'Запуск сообщения по команде /start_3 используя магический фильтр F.text! {message.from_user.id} {message.from_user.username} {message.from_user.first_name} {message.from_user.last_name}',
+        reply_markup=create_rat()
+    )
+
+
+async def set_commands():
+    commands = [BotCommand(command='start', description='Старт'),
+                BotCommand(command='start_2', description='Старт 2'),
+                BotCommand(command='start_3', description='Старт 3')]
+    await bot.set_my_commands(commands, BotCommandScopeDefault())
